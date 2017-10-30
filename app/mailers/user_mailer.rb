10862fd5ce51b3e5f279f5a545b7250f5ec18040
Maxime Boue <abriko@yahoo.fr>
2017-10-30 15:04:15 +0100

    Add Guest email to deliver

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def welcome(guest)
     @user = guest
     @url  = 'http://www.parentheze.com'
-    mail(to: "maxime@parentheze.com", from: "maxime@parentheze.com", subject: 'Inscription à la béta de parentheze', :track_opens => 'true')
+    mail(to: @user.email, from: "maxime@parentheze.com", subject: 'Inscription à la béta de parentheze', :track_opens => 'true')
   end
 
 end
