84ae2123fead63b143cc658e16b0f0cd1f3e135e
Maxime Boue <abriko@yahoo.fr>
2017-10-30 16:09:39 +0100

    Add redirection and welcome view

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,14 +1,18 @@
 class GuestsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create, :show]
 
   def create
     @guest = Guest.new(params_guest)
     if @guest.save!
       UserMailer.welcome(@guest).deliver_now
-      flash.now[:alert] = t('flash.guest.new')
+      render :welcome
     end
-
   end
+
+  def welcome
+   @guest = Guest.find_by_email(params[:guest][:email])
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "/home" => "pages#home"
+
+  # MAIL
+  get "/welcome_redirection" => "guests#welcome_redirection"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
