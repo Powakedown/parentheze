e9582a9e1b5b8b74e4d4124e7761a3c92b5b8e4d
Powakedown <abriko@yahoo.fr>
2017-10-31 12:16:41 +0100

    Delete guest controller

--- test/controllers/guests_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TestersControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get testers_create_url
-    assert_response :success
-  end
-
-end
