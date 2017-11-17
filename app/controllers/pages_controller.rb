1a7a33cfa69bd079fb3874f4486eff1a37b69a3b
Powakedown <abriko@yahoo.fr>
2017-11-17 16:34:48 +0100

    fix form for email on home and survey

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @guest = Guest.new
+    @guest = guest_user
   end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,3 @@
 class Guest < ApplicationRecord
   validates :email, presence: { message: "ne peut pas être vide" }, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/, message: "non valide" }
-
 end
