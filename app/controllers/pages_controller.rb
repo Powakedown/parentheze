bb29adc7161f46e6c54147cb9fccc2e1ce01f7d0
Maxime Boue <abriko@yahoo.fr>
2017-11-21 11:49:32 +0100

    Add cookie to skip survey on second visit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @guest = guest_user
+    if cookies[:parentheze_guest] && cookies[:parentheze_guest] != ""
+      @guest = guest_user
+    else
+      redirect_to new_guest_path
+    end
   end
 end
