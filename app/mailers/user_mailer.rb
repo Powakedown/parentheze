883dc5d26b059a337878faa4b6bb271d359521d6
Powakedown <abriko@yahoo.fr>
2017-11-30 18:34:00 +0100

    start adding view, add controller for duplicate mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,19 @@
 class UserMailer < ApplicationMailer
-  default from: 'parentgenial@parentheze.com'
+  default from: 'noreply@parentheze.com'
 
   def welcome(guest)
     @user = guest
     @url  = 'http://www.parentheze.com'
-    mail(to: @user.email, from: "parentgenial@parentheze.com", subject: 'Inscription à la béta de parentheze', :track_opens => 'true')
+    mail(to: @user.email, from: "noreply@parentheze.com", subject: 'Inscription à la béta de parentheze', :track_opens => 'true')
+    self_notification(guest)
   end
 
   def welcome_redirection(guest)
     @user = guest
   end
+
+  def self_notification
+    mail(to: "abriko@yahoo.fr", from: "noreply@parentheze.com", subject: 'Nouveau Guest sur Parentheze', :track_opens => 'true')
+  end
+
 end
