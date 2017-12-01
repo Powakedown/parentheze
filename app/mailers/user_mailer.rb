8d7c9ecda99e54d6d949ceb4b9ec63731506ec32
Powakedown <abriko@yahoo.fr>
2017-12-01 10:40:53 +0100

    Enhance Guest index controller, adding scope to model

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: 'noreply@parentheze.com'
+  default from: 'parentgenial@parentheze.com'
 
   def welcome(guest)
     @user = guest
     @url  = 'http://www.parentheze.com'
-    mail(to: @user.email, from: "noreply@parentheze.com", subject: 'Inscription à la béta de parentheze', :track_opens => 'true')
+    mail(to: @user.email, from: "parentgenial@parentheze.com", subject: 'Inscription à la béta de parentheze', :track_opens => 'true')
     self_notification(guest)
   end
 
@@ -12,8 +12,9 @@ class UserMailer < ApplicationMailer
     @user = guest
   end
 
-  def self_notification
-    mail(to: "abriko@yahoo.fr", from: "noreply@parentheze.com", subject: 'Nouveau Guest sur Parentheze', :track_opens => 'true')
+  def self_notification(guest)
+    @user = guest
+    mail(to: "abriko@yahoo.fr", from: "parentgenial@parentheze.com", subject: 'Nouveau Guest sur Parentheze', :track_opens => 'true')
   end
 
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,11 @@ class Guest < ApplicationRecord
   validates :email, presence: { message: "ne peut pas être vide" },
                     format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/, message: "non valide" }
   validates_uniqueness_of :email, conditions: -> { where.not(email: 'email@example.com') }
+
+  scope :parenting, -> { where(parent: 1)}
+  scope :with_kid_in_age, -> { where(kid_age: 1)}
+  scope :with_jalousy, -> { where( jalous: [1,2])}
+  scope :want_to_get_out, -> { where(get_out: [1,2])}
+  scope :visitors, -> { where.not(visit: 0)}
+  scope :subscriber, -> { where.not(email: "email@example.com")}
 end
