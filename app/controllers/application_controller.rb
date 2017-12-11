db85c08a330e76864504bb1f59b9abdd273d79a0
Powakedown <abriko@yahoo.fr>
2017-12-11 12:38:02 +0100

    Cookie is working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def guest_user(with_retry = true)
     # Cache the value the first time it's gotten.
-    @cached_guest_user ||= Guest.find(session[:guest_user_id] ||= create_guest_user.id)
+    @cached_guest_user ||= Guest.find(session[:guest_user_id] || cookies[:parentheze_guest] ||= create_guest_user.id)
   rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
     session[:guest_user_id] = nil
     guest_user if with_retry
@@ -17,11 +17,15 @@ class ApplicationController < ActionController::Base
     u = Guest.new(name: 'guest', email: 'email@example.com')
     u.save!(validate: false)
     session[:guest_user_id] = u.id
+    cookies[:parentheze_guest] = {
+      value: u.id,
+      expires: 1.year.from_now
+    }
     u
   end
 
   def cookie
-    cookies[:parentheze_guest].present?
+    @cookie = cookies[:parentheze_guest].present?
   end
 
   def average(collection, column)
