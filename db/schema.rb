950f565bb7ef12844ddcc1e0da6e9f835415b575
Maxime Boue <abriko@yahoo.fr>
2017-12-13 12:53:55 +0100

    correct errors on text, split text in yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211095944) do
+ActiveRecord::Schema.define(version: 20171206143106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
