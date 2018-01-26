1220930d8cb908d92ef2b8fcb98f87a49e9f5033
Powakedown <abriko@yahoo.fr>
2018-01-26 10:29:08 +0100

    Add javascript handling breadcrumb active step

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def inscription
     @user = current_user
+    @current_step = 2
     @signup_length = 5
   end
 
@@ -9,6 +10,7 @@ class UsersController < ApplicationController
   end
 
   def update
-
+    raise
+    @current_step += 1
   end
 end
