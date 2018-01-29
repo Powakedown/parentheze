5717205ae4c3a1d0491c40e2f42d78e363162aeb
Powakedown <abriko@yahoo.fr>
2018-01-29 14:23:52 +0100

    Dsitribute .js to asset pipeline

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    $("#animatedElement").each(function(){
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+400) {
+        $(this).addClass("slideUp");
+      }
+    });
+  });
+
+  $(document).on("click", "a.scroll", function(event){
+    event.preventDefault();
+
+    $("html, body").animate({
+        scrollTop: $( $.attr(this, "href") ).offset().top
+    }, 500);
+  });
+});
