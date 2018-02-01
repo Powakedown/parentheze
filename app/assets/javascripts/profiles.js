ef7f1081ceccbbeaac45d104f8bcf869378f0a9d
Powakedown <abriko@yahoo.fr>
2018-02-01 17:23:14 +0100

    Add google map API, change field on javascript >> autocomplete is working, TO DO >> add field to validate the right address

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,43 +1,78 @@
-//   window.onload = function() {
-//   var current_step = 2;
-//   target_step = document.getElementById('breadcrumb-step'+current_step)
-//   target_step.className += " active";
-
-//   let submits = document.getElementsByClassName( 'login-button' );
-//   let comebacks = document.getElementsByClassName( 'previous' );
-
-//   for (i = 2; i <= 5 ; i++) {
-//     if( i != current_step){
-//       document.getElementById('screen'+i).style.display = 'none';
-//     }
-//   }
-
-//   for ( let comeback of comebacks) {
-//     comeback.onclick = function() {
-//       step(this,-1)
-//     }
-//   }
-
-//   for ( let submit of submits ) {
-//     submit.onclick = function() {
-//       step(this, 1);
-//     }
-//   }
-
-//   var step = function(button, target) {
-//     current_step = button.id.charAt(button.id.length-1)
-//     target_step_dot = document.getElementById('breadcrumb-step'+(parseInt(current_step)+target))
-//     current_step_dot = document.getElementById('breadcrumb-step'+parseInt(current_step))
-//     target_step_dot.className = "step flexbox active";
-//     current_step_dot.className = "step flexbox";
-
-//     document.getElementById('screen'+current_step).style.display = 'none';
-//     document.getElementById('screen'+(parseInt(current_step)+target)).style.display = 'block';
-//   }
-// };
-
 $(document).ready(function() {
   $('.attachinary-input').attachinary();
+});
+
+function onPlaceChanged() {
+  var place = this.getPlace();
+  var components = getAddressComponents(place);
+
+  var profileAddress = document.getElementById('profile_address');
+  profileAddress.blur();
+  profileAddress.value = components.address;
+
+  document.getElementById('profile_zip_code').value = components.zip_code;
+  document.getElementById('profile_city').value = components.city;
+
+  if (components.country_code) {
+    var selector = '#profile_country option[value="' + components.country_code + '"]';
+    document.querySelector(selector).selected = true;
+  }
+}
+
+function getAddressComponents(place) {
+  // If you want lat/lng, you can look at:
+  // - place.geometry.location.lat()
+  // - place.geometry.location.lng()
+
+  if (window.console && typeof console.log === "function") {
+    console.log(place);
+  }
+
+  var street_number = null;
+  var route = null;
+  var zip_code = null;
+  var city = null;
+  var country_code = null;
+  for (var i in place.address_components) {
+    var component = place.address_components[i];
+    for (var j in component.types) {
+      var type = component.types[j];
+      if (type === 'street_number') {
+        street_number = component.long_name;
+      } else if (type === 'route') {
+        route = component.long_name;
+      } else if (type === 'postal_code') {
+        zip_code = component.long_name;
+      } else if (type === 'locality') {
+        city = component.long_name;
+      } else if (type === 'postal_town' && city === null) {
+        city = component.long_name;
+      } else if (type === 'country') {
+        country_code = component.short_name;
+      }
+    }
+  }
+
+  return {
+    address: street_number === null ? route : (street_number + ' ' + route),
+    zip_code: zip_code,
+    city: city,
+    country_code: country_code
+  };
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var profileAddress = document.getElementById('profile_address');
+
+  if (profileAddress) {
+    var autocomplete = new google.maps.places.Autocomplete(profileAddress, { types: ['geocode'] });
+    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+    google.maps.event.addDomListener(profileAddress, 'keydown', function(e) {
+      if (e.key === "Enter") {
+        e.preventDefault(); // Do not submit the form on Enter.
+      }
+    });
+  }
 });
 
 
