9c12105ce1a700d75ef9e6b2098387dbf012441e
Powakedown <abriko@yahoo.fr>
2018-02-14 16:09:38 +0100

    Create new registration view, partial it, new logo with purple

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -23,4 +23,13 @@ class UserMailer < ApplicationMailer
          subject: 'Nouveau Guest sur Parentheze',
          track_opens: 'true')
   end
+
+  def new_registration(user)
+    @user = user
+    @url  = 'http://www.parentheze.com'
+    mail(to: @user.email,
+         from: 'parentgenial@parentheze.com',
+         subject: 'Inscription à la béta de parentheze',
+         track_opens: 'true')
+  end
 end
