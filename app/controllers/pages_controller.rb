1ec1a5ab3f21dc7a91736495fc95158103c95b60
Powakedown <abriko@yahoo.fr>
2018-03-06 17:52:14 +0100

    Fix manifeste on bootstrap issue, format title on contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,20 +23,16 @@ class PagesController < ApplicationController
 
   def contact
     @message = params_message
-    mail_regex = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/
-    @valid_email = @message[:email] =~ mail_regex && @message[:email].present?
-    @valid_comment = @message[:comment].present? && @message[:comment].length > 8
+    @valid_email = email_valid(@message[:email])
+    @valid_comment = comment_valid(@message[:comment])
     @human = params[:iamhuman].present?
 
     if @valid_email && @valid_comment && @human
-
       session[:contact_email] = session[:contact_name] = session[:contact_comment] = nil
       flash[:notice] = t('.notice')
       UserMailer.contact_form(@message[:name], @message[:email], @message[:comment]).deliver_now
       redirect_to root_path
-
     else
-
       session[:contact_email] = @message[:email]
       session[:contact_name] = @message[:name]
       session[:contact_comment] = @message[:comment]
@@ -45,7 +41,6 @@ class PagesController < ApplicationController
       flash[:alert] = t('.blank_email') unless @email_presence
       flash[:alert] = t('.invalid_comment') unless @valid_comment
       redirect_to "/#slide4"
-
     end
   end
 
