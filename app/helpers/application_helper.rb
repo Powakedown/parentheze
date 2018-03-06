2e45f6f41daedd11c4b738e37b90358d7db0f900
Powakedown <abriko@yahoo.fr>
2018-03-06 22:27:46 +0100

    Add helper to display right icon on flash popup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_icon(key)
+    key[-1].to_i != 0 ? key = key.chop : key
+    case key
+    when "warning"
+      return "fas fa-exclamation-triangle"
+    when "alert"
+      return "fas fa-exclamation-circle"
+    when "notice"
+      return "far fa-bell"
+    when "success"
+      return "far fa-thumbs-up"
+    end
+  end
 end
