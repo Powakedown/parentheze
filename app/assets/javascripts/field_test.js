af5c33fa641fe864b75a6bf4ae30d4f24200950e
Powakedown <abriko@yahoo.fr>
2018-05-04 12:23:45 +0200

    Refacto js regex and keydown to keyup

--- app/assets/javascripts/field_test.js
+++ app/assets/javascripts/field_test.js
@@ -1,33 +1,43 @@
-var patternEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-var patternEmails = /^[\W]*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]{2,4}[\W]*;{1}[\W]*)*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]{2,4})[\W]*$/
-var patternName = "^[^0-9][\\w'\\-áéíóúäëïöüÄőŐűŰZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÉÈÊĒÀÄ][^0-9_!.¡?÷?¿+=@#$%^&*(){}|~<>;:\\[\\]]{0,}$";
-var patternNumber = "^\\d+$";
-var patternPhone = "^(0|\\+33|0033)[1-9][0-9]{8}$";
+const patternEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+const patternIdForEmail = /^([a-zA-Z0-9_\.\-])+/;
+const patternEmails = /^[\W]*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]{2,4}[\W]*;{1}[\W]*)*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]{2,4})[\W]*$/
+const patternName = "^[^0-9][\\w'\\-áéíóúäëïöüÄőŐűŰZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÉÈÊĒÀÄ][^0-9_!.¡?÷?¿+=@#$%^&*(){}|~<>;:\\[\\]]{0,}$";
+const patternNumber = "^\\d+$";
+const patternPhone = "^(0|\\+33|0033)[1-9][0-9]{8}$";
 
 function testMail(e) {
-  console.log("testMail");
-  var input = this;
-  input.style.color = "light-gray";
+  this.style.color = "light-gray";
 
-  setTimeout(function(){
-    if(input.value.match(patternEmails)){
-      input.style.cssText = null;
+  if(this.value.match(patternEmails)){
+    this.style.cssText = null;
+  }
+  else if (!this.value.includes('@')) {
+    if (this.value.match(patternIdForEmail)) {
+      this.style.cssText = null;
     }
-    else if (input.value.length > 5) {
-      input.style.borderColor = "red";
-      input.style.color = "red";
+    else {
+      this.style.borderColor = "red";
+      this.style.color = "red";
     }
-    else if (input.value.length == 0 ) {
-      input.style.cssText = null;
+  }
+  else if (this.value.includes('@')) {
+    if (this.value.match(patternEmail)) {
+      this.style.cssText = null;
     }
-  }, 10);
+    else {
+      this.style.borderColor = "red";
+      this.style.color = "red";
+    }
+  }
+  else if (this.value.length == 0 ) {
+    this.style.cssText = null;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  var profileEmail = document.getElementById('user_email');
+  const profileEmail = document.getElementById('user_email');
 
   if (profileEmail) {
-    profileEmail.addEventListener("keydown", testMail, false);
+    profileEmail.addEventListener("keyup", testMail, false);
   }
-
 });
